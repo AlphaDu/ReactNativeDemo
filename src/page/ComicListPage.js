f95b0ea210ec4d86d5c2cd928c366f36ca3b01bb
杜亚凡 <alphadu811@outlook.com>
2017-04-09 23:35:45 +0800

    Master

--- src/page/ComicListPage.js
+++ src/page/ComicListPage.js
@@ -10,12 +10,21 @@ import {
     ScrollView,
     ActivityIndicator
 } from 'react-native'
+import CookieManager from 'react-native-cookies'
 import ListPager from '../components/ListPager'
 import mockdata from '../mock/comiclist';
-let model = ['a','b','c','e','f','g','h'];
+let model = ['a', 'b', 'c', 'e', 'f', 'g', 'h'];
+// CookieManager.setFromResponse('https://exhentai.org/', 'ipb_member_id=2390332; ipb_pass_hash=e9532dec36afd6d54eafa8ffb2e14168; igneous=aacb6fb36; s=1f6b1f494; uconfig=dm_t; lv=1491307778-1491735249', (res) => {
+//     // `res` will be true or false depending on success.
+//     console.log("Set cookie", res);
+// });
+CookieManager.get('https://exhentai.org/', (err, res) => {
+    console.log('Got cookies for url', res);
+    // Outputs 'user_session=abcdefg; path=/;'
+});
 
-export default class ComicListPage extends Component{
-    constructor (props) {
+export default class ComicListPage extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             dataSource: new ListView.DataSource({
@@ -24,18 +33,20 @@ export default class ComicListPage extends Component{
             text: ''
         };
     }
-    _renderRow = (data)=>{
+
+    _renderRow = (data) => {
         return (
             <ListCell {...data}/>
         )
     };
-    goBack = () =>{
+    goBack = () => {
 
     };
-    goFoward = () =>{
+    goFoward = () => {
 
     };
-    render(){
+
+    render() {
         return (
             <View style={{flex:1,flexDirection:'column'}}>
                 <View style={{flex:0.5,alignItems: 'center'}}>
@@ -53,24 +64,23 @@ export default class ComicListPage extends Component{
     };
 }
 const header = {
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate, sdch, br",
-    "Accept-Language":"zh-CN,zh;q=0.8",
-    "Cache-Control":"max-age=0",
-    "Connection":"keep-alive",
-    "Cookie":"ipb_member_id=2390332; ipb_pass_hash=e9532dec36afd6d54eafa8ffb2e14168; igneous=aacb6fb36; s=1f6b1f494; uconfig=dm_t; lv=1491288799-1491307778",
-    "Host":"exhentai.org",
-    "Referer":"https://exhentai.org/s/a10270b82f/1045863-4",
-    "Upgrade-Insecure-Requests":"1",
-    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch, br",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+     "Host": "exhentai.org",
+    "Referer": "https://exhentai.org/s/a10270b82f/1045863-4",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
 };
-const ListCell = (props)=> {
+const ListCell = (props) => {
     return (
         <View>
             <View style={{flexDirection:'row',height:90}}>
                 <View style={{flex:2}}>
                     <Image source={{url:props.cover,
-                    headers:header}}/>
+                    headers:header}} style={{width:'100%',height:'100%'}}/>
                 </View>
                 <View style={{flex:8}}>
                     <Text>wocao{props.title}</Text>
